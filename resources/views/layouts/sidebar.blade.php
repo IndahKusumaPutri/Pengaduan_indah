c98575b8074729504b97e5652b94ca4051b7c2d8
IndahKusumaPutri <indahkusumaputri76@gmail.com>
2023-03-01 23:06:42 +0700

    update 01/03

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,60 +1,30 @@
-<div class="sidebar" data-color="purple" data-image="assets/img/sidebar-5.jpg">
-	<div class="sidebar-wrapper">
+<div class="sidebar" data-image="{{ asset('img/sidebar-5.jpg') }}">
+    <div class="sidebar-wrapper">
         <div class="logo">
-            <a href="http://www.creative-tim.com" class="simple-text">
-                Creative Tim
+            <a href="javascript:;" class="simple-text">
+                Your Logo
             </a>
         </div>
-
         <ul class="nav">
-            <li class="active">
-                <a href="dashboard.html">
-                    <i class="pe-7s-graph"></i>
-                    <p>Dashboard</p>
+            <li class="nav-item active">
+                <a class="nav-link" href="dashboard.html">
+                    <i class="nc-icon nc-icon nc-paper-2"></i>
+                    <p>First example</p>
                 </a>
             </li>
             <li>
-                <a href="user.html">
-                    <i class="pe-7s-user"></i>
-                    <p>User Profile</p>
+                <a class="nav-link" href="./user.html">
+                    <i class="nc-icon nc-bell-55"></i>
+                    <p>Second example</p>
                 </a>
             </li>
-            <li>
-                <a href="table.html">
-                    <i class="pe-7s-note2"></i>
-                    <p>Table List</p>
-                </a>
-            </li>
-            <li>
-                <a href="typography.html">
-                    <i class="pe-7s-news-paper"></i>
-                    <p>Typography</p>
-                </a>
-            </li>
-            <li>
-                <a href="icons.html">
-                    <i class="pe-7s-science"></i>
-                    <p>Icons</p>
-                </a>
-            </li>
-            <li>
-                <a href="maps.html">
-                    <i class="pe-7s-map-marker"></i>
-                    <p>Maps</p>
-                </a>
-            </li>
-            <li>
-                <a href="notifications.html">
-                    <i class="pe-7s-bell"></i>
-                    <p>Notifications</p>
-                </a>
-            </li>
-			<li class="active-pro">
-                <a href="upgrade.html">
-                    <i class="pe-7s-rocket"></i>
-                    <p>Upgrade to PRO</p>
+
+            <li class="nav-item active active-pro">
+                <a class="nav-link active" href="javascript:;">
+                    <i class="nc-icon nc-alien-33"></i>
+                    <p>Upgrade plan</p>
                 </a>
             </li>
         </ul>
-	</div>
+    </div>
 </div>
