0601bdcdc9ab2501709c0558dca907cf04ec1741
IndahKusumaPutri <indahkusumaputri76@gmail.com>
2023-03-03 08:38:15 +0700

    Update 03/03

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,16 +9,17 @@ class HomeController extends Controller
 {
     public function index()
     {
-        //$pinjam = date("06-09-2022");
-        $tujuh_hari = mktime(0,0,0, date("n"), date("j")+30, date("Y"));
-        $kembali = date("d-m-Y", $tujuh_hari);
+        return view('/home');
+        // //$pinjam = date("06-09-2022");
+        // $tujuh_hari = mktime(0,0,0, date("n"), date("j")+30, date("Y"));
+        // $kembali = date("d-m-Y", $tujuh_hari);
 
-        $jumlah_tanggapan = Tanggapan::count();
-        $jumlah_pengaduan = Pengaduan::count();
+        // $jumlah_tanggapan = Tanggapan::count();
+        // $jumlah_pengaduan = Pengaduan::count();
         
-        return view('beranda.home')
-        ->with('jumlah_tanggapan',$jumlah_tanggapan)
-        ->with('jumlah_pengaduan',$jumlah_pengaduan)
-        ->with('kembali',$kembali);
+        // return view('home')
+        // ->with('jumlah_tanggapan',$jumlah_tanggapan)
+        // ->with('jumlah_pengaduan',$jumlah_pengaduan)
+        // ->with('kembali',$kembali);
     }
 }
--- resources/views/beranda/home.blade.php
+++ resources/views/beranda/home.blade.php
@@ -23,14 +23,14 @@
                 <div class="col-lg-3 col-6">
                     <div class="small-box bg-info">
                         <div class="inner">
-                            <h3>{{ $jumlah_kader }}</h3>
+                            <!-- <h3>{{ $jumlah_kader }}</h3> -->
 
                             <p>Data Tugas Kader</p>
                         </div>
                         <div class="icon">
                             <i class="fas fa-solid fa-briefcase"></i>
                         </div>
-                        <a href="{{ route('tugas.index') }}" class="small-box-footer">Info Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="#" class="small-box-footer">Info Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <!-- ./col -->
@@ -38,14 +38,14 @@
                 <div class="col-lg-3 col-6">
                     <div class="small-box bg-warning">
                         <div class="inner">
-                            <h3>{{ $jumlah_kader }}</h3>
+                            <!-- <h3>{{ $jumlah_kader }}</h3> -->
 
                             <p>Data Kader</p>
                         </div>
                         <div class="icon">
                             <i class="fas fa-user"></i>
                         </div>
-                        <a href="{{ route('kader.index') }}" class="small-box-footer">Info Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="#" class="small-box-footer">Info Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <!-- ./col -->
@@ -53,14 +53,14 @@
                 <div class="col-lg-3 col-6">
                     <div class="small-box bg-success">
                         <div class="inner">
-                            <h3>{{ $jumlah_vaksin }}</h3>
+                            <!-- <h3>{{ $jumlah_vaksin }}</h3> -->
 
                             <p>Data Vaksin & Vitamin</p>
                         </div>
                         <div class="icon">
                             <i class="fas fa-shield-virus"></i>
                         </div>
-                        <a href="{{ route('vaksin.index') }}" class="small-box-footer">Info Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="#" class="small-box-footer">Info Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <!-- ./col -->
@@ -68,13 +68,13 @@
                 <div class="col-lg-3 col-6">
                     <div class="small-box bg-danger">
                         <div class="inner">
-                            <h3>{{ $jumlah_ibu }}</h3>
+                            <!-- <h3>{{ $jumlah_ibu }}</h3> -->
                             <p>Data Ibu</p>
                         </div>
                         <div class="icon">
                             <i class="fas fa-female"></i>
                         </div>
-                        <a href="{{ route('ibu.index') }}" class="small-box-footer">Info Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="#" class="small-box-footer">Info Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <!-- ./col -->
@@ -82,14 +82,14 @@
                 <div class="col-lg-3 col-6">
                     <div class="small-box bg-info">
                         <div class="inner">
-                            <h3>{{ $jumlah_pasien }}</h3>
+                            <!-- <h3>{{ $jumlah_pasien }}</h3> -->
 
                             <p>Data Balita</p>
                         </div>
                         <div class="icon">
                             <i class="fas fa-baby"></i>
                         </div>
-                        <a href="{{ route('pasien.index') }}" class="small-box-footer">Info Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="" class="small-box-footer">Info Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <!-- ./col -->
@@ -97,14 +97,14 @@
                 <div class="col-lg-3 col-6">
                     <div class="small-box bg-warning">
                         <div class="inner">
-                            <h3>{{ $jumlah_nimbang }}</h3>
+                            <!-- <h3>{{ $jumlah_nimbang }}</h3> -->
 
                             <p>Penimbangan Balita</p>
                         </div>
                         <div class="icon">
                             <i class="fas fa-weight"></i>
                         </div>
-                        <a href="{{ route('nimbang.index') }}" class="small-box-footer">Info Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="#" class="small-box-footer">Info Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <!-- ./col -->
@@ -112,14 +112,14 @@
                 <div class="col-lg-3 col-6">
                     <div class="small-box bg-success">
                         <div class="inner">
-                            <h3>{{ $jumlah_imunisasi }}</h3>
+                            <!-- <h3>{{ $jumlah_imunisasi }}</h3> -->
 
                             <p>Imunisasi Balita</p>
                         </div>
                         <div class="icon">
                             <i class="fas fa-solid fa-syringe"></i>
                         </div>
-                        <a href="{{ route('imunisasi.index') }}" class="small-box-footer">Info Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="#" class="small-box-footer">Info Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <!-- ./col -->
